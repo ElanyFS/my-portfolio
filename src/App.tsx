68439d9be205b5fddc0bde68c8bc57c5ab4db6a4
Elany Ferreira <ellanysouza08@gmail.com>
2024-11-29 10:47:23 -0300

    feature/update loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import { Layout } from "./Layout/Layout";
-import { Loading } from "./components/Loading/Loading";
-import './style/index.css';
+// import { Loading } from "./components/Loading/Loading";
+import "./style/index.css";
 
 function App() {
-  const [loading, setLoading] = useState<boolean>(true);
+  // const [loading, setLoading] = useState<boolean>(true);
 
-  useEffect(()=>{
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setLoading(false);
+  //   }, 6000);
 
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 6000);
+  //   return () => clearTimeout(timer);
+  // }, []);
 
-    return () => clearTimeout(timer)
-  }, [])
-
-  return <>
-    {loading ? 
-    <Loading /> : 
-    <div className={`page-content`}>
+  return (
+    <>
+      {/* {loading ?  */}
+      {/* <Loading /> :  */}
+      {/* <div className={`page-content`}> */}
       <Layout />
-    </div>
-     }
-  </>;
+      {/* </div> */}
+      {/* }; */}
+    </>
+  );
 }
 
 export default App;
